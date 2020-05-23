48172b76f3efa53a0b8b4c7148f24dadd1a08a79
Alessio <github@alexpaulofficial.com>
2020-05-23 12:42:25 +0200

    Inizializzazione
    Creazione progetto base Spring Boot

---
+++ src/main/java/it/univpm/GiAle/twitterProj/app/TwitterProjectApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.GiAle.twitterProj.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterProjectApplication.class, args);
+	}
+
+}
