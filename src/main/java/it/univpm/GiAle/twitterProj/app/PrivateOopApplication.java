9e4c84ae1f2104f0918bcdb7486f8c8f10a949a0
Alessio <github@alexpaulofficial.com>
2020-05-23 12:27:18 +0200

    Creazione base Spring Boot
    Creazione iniziale del progetto

---
+++ src/main/java/it/univpm/GiAle/twitterProj/app/PrivateOopApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.GiAle.twitterProj.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateOopApplication.class, args);
+	}
+
+}
