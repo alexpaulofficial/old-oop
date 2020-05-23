6700f413b73f3dc6f2c9b642e340a72bfb4894a8
Alessio <github@alexpaulofficial.com>
2020-05-23 13:08:53 +0200

    Aggiunta dei package
    Sono stati aggiunti i principali package con le corrispettive classi di base

--- src/main/java/it/univpm/GiAle/twitterProj/app/TwitterProjectApplication.java
+++ src/main/java/it/univpm/GiAle/twitterProj/app/TwitterProjectApplication.java
@@ -9,5 +9,5 @@ public class TwitterProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TwitterProjectApplication.class, args);
 	}
-
+	
 }
---
+++ src/main/java/it/univpm/GiAle/twitterProj/controller/AppController.java
@@ -0,0 +1,5 @@
+package it.univpm.GiAle.twitterProj.controller;
+
+public class AppController {
+
+}
