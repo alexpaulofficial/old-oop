6d73b711007633f3e8d2f198d404c9d65b8ac334
Alessio <github@alexpaulofficial.com>
2020-06-11 12:54:27 +0200

    Vari cambiamenti Controller
    Molti cambiamenti per le nuove funzioni di Filter e di Stats, tra cui la famosa correzione del metodo con il nome sbagliato

--- src/main/java/it/univpm/GiAle/twitterProj/controller/AppController.java
+++ src/main/java/it/univpm/GiAle/twitterProj/controller/AppController.java
@@ -1,8 +1,11 @@
 package it.univpm.GiAle.twitterProj.controller;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.text.ParseException;
 
 import javax.swing.text.html.parser.Entity;
+import javax.xml.ws.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,9 +13,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -24,6 +29,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import it.univpm.GiAle.twitterProj.filters.Filter;
+import it.univpm.GiAle.twitterProj.filters.Stats;
 import it.univpm.GiAle.twitterProj.model.Tweet;
 import it.univpm.GiAle.twitterProj.service.TweetService;
 
@@ -31,62 +37,49 @@ import it.univpm.GiAle.twitterProj.service.TweetService;
 public class AppController {
 	@Autowired
 	TweetService service;
-	@RequestMapping (value = "/data", method = RequestMethod.GET)
-	public ResponseEntity<Object> getTweet () {
-		return new ResponseEntity<> (service.getTweet(), HttpStatus.OK);
+
+	@RequestMapping(value = "/data", method = RequestMethod.GET)
+	public ResponseEntity<Object> getTweet() {
+		return new ResponseEntity<>(service.getTweet(), HttpStatus.OK);
 	}
 
 	@PostMapping("/data")
-	public ResponseEntity<Object> exampleMethod2 (@RequestBody String body) {
-		
+	public ResponseEntity<Object> postJSONBody(@RequestBody String body) {
+
 		service.addTweetsArray(service.addJson(body));
-		return new ResponseEntity<> ("Tweet caricati correttamente. Per verificare, fare una richiesta GET allo stesso indirizzo", HttpStatus.OK);
+		return new ResponseEntity<>(
+				"Tweet caricati correttamente. Per verificare, fare una richiesta GET allo stesso indirizzo",
+				HttpStatus.OK);
 	}
+	@PostMapping("/data/twitter")
+	public ResponseEntity<Object> postFromTwitter(@RequestBody String url) throws MalformedURLException, IOException {
+		
+		service.addTweetsArray(service.addJson(service.GetFromTwitter(url)));
+		return new ResponseEntity<>(
+				"Tweet scaricati correttamente da twitter. Per verificare, fare una richiesta GET allo stesso indirizzo",
+				HttpStatus.OK);
+	}
+
 	@GetMapping("/data/filter")
-	public ResponseEntity<Object> filter (@RequestBody String bodyFilter) {
-		JsonObject gson = new Gson().fromJson(bodyFilter, JsonObject.class);
-		String filterFiled = gson.get("filter_field").getAsString();
-		String filterType = gson.get("filter_type").getAsString();
-		JsonElement param = gson.get("parameters");
-		ResponseEntity<Object> Entity = new ResponseEntity<Object>(HttpStatus.ACCEPTED);
-		if (filterFiled.equals("likes")) {
-			if (Filter.filterByLikes(service.getTweet(), filterType, param) == null) 
-			{
-				Entity = new ResponseEntity<Object>("Errore!",HttpStatus.BAD_REQUEST);
-			}
-			else
-				Entity = new ResponseEntity<Object>(Filter.filterByLikes(service.getTweet(), filterType, param), HttpStatus.OK);
-
-		}
-		if (filterFiled.equals("retweets")) {
-			if (Filter.filterByRetweet(service.getTweet(), filterType, param) == null) 
-			{
-				Entity = new ResponseEntity<Object>("Errore!",HttpStatus.BAD_REQUEST);
-			}
-			else
-				Entity = new ResponseEntity<Object>(Filter.filterByRetweet(service.getTweet(), filterType, param), HttpStatus.OK);
-
-		}
-		if (filterFiled.equals("time")) {
-			try {
-				Entity = new ResponseEntity<Object>(Filter.filterByTime(service.getTweet(), filterType, param), HttpStatus.OK);
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		return Entity;
+	public ResponseEntity<Object> filter(@RequestBody String bodyFilter) {
+		
+		return new ResponseEntity<Object> (service.filtraggio(bodyFilter, service.getTweet()), HttpStatus.OK);
 	}
-	
-	@RequestMapping(value = "/metadata", method = RequestMethod.GET, produces="application/json")
-    String getMetadata(){
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            JsonSchemaGenerator schemaGen = new JsonSchemaGenerator(mapper);
-            com.fasterxml.jackson.module.jsonSchema.JsonSchema schema = schemaGen.generateSchema(Tweet.class);
-            return mapper.writeValueAsString(schema);
-        } catch (JsonProcessingException e){
-        	return e.getLocalizedMessage();
-        }
-    }
+
+	@GetMapping("/data/stats")
+	public ResponseEntity<Object> stats(@RequestBody String bodyFilter) {
+		return new ResponseEntity<Object> (Stats.stats(service.getTweet()), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/metadata", method = RequestMethod.GET, produces = "application/json")
+	String getMetadata() {
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			JsonSchemaGenerator schemaGen = new JsonSchemaGenerator(mapper);
+			com.fasterxml.jackson.module.jsonSchema.JsonSchema schema = schemaGen.generateSchema(Tweet.class);
+			return mapper.writeValueAsString(schema);
+		} catch (JsonProcessingException e) {
+			return e.getLocalizedMessage();
+		}
+	}
 }
